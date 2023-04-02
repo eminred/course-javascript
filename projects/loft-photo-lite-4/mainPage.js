c5d45a4c0a4e209af02ab8d6c5a9ceceb9269bc3
Emin <eminsamedovred@gmail.com>
2023-04-02 12:17:29 +0300

    Tasks #5 - cookies and project profilePage

--- projects/loft-photo-lite-4/mainPage.js
+++ projects/loft-photo-lite-4/mainPage.js
@@ -9,7 +9,47 @@ export default {
   },
 
   setFriendAndPhoto(friend, id, url) {
+    const photoComp = document.querySelector('.component-photo');
+    const headerphotoComp = document.querySelector('.component-header-photo');
+    const headerNameComp = document.querySelector('.component-header-name');
+    const footerPhotoComp = document.querySelector ('.component-footer-photo');
+
+    this.friend = friend;
+
+    headerphotoComp.style.backgroundimage = `url('${friend.photo_50}')`;
+    headerNameComp.innerHTML = `${friend.first_name ?? ''} ${friend.last_name ?? ''}`;
+    photoComp.style.backgroundimage = `url(${url})`;
+    footerPhotoComp.style.backgroundimage = `url('${model.me.photo_50}')`;
   },
 
   handleEvents() {
+    let startFrom; 
+
+    document.querySelector('.component-photo').addEventListener('touchstart', (e) => {
+      e.preventDefault();
+      startFrom = { y: e.changedTouches[0].pageY};
+    });
+
+    document.querySelector('.component-photo').addEventListener('touched', async (e) => {
+      const direction = e.changedTouches[0].pageY - startFrom.y;
+
+      if (direction < 0) {
+        await this.getNextPhoto();
+      }
+    });
+
+    document
+    .querySelector('.component-header-profile-link')
+    .addEventListener('click', async () => {
+      await profilePage.setUser(this.friend);
+      pages.openPage('profile');
+    });
+
+    document 
+    .querySelector('.component-footer-container-profile-link')
+    .addEventListener('click', async () => {
+      await profilePage.setUser(model.me);
+      pages.openPage('profile');
+    });
+  },
 };
